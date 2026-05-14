f26f98802c42cb05114b9f34ab7b2d5b4bdf28db
HaseebullahMiakhil <tw773029@gmail.com>
2026-05-14 14:05:47 +0430

    welcomeel

---
+++ welcome.js
@@ -0,0 +1,8 @@
+let welcomeEl = document.getElementById("welcome-el")
+
+let name = "haseebullah miakhil"
+let greeting = "welcome back!"
+
+welcomeEl.innerText = greeting + name
+
+welcomeEl.innerText = welcomeEl.innerText +"👋"
